6ab9332001bb4cd6360069ecdf180e27d2a62e42
loh-chunheng <chunheng.loh@gmail.com>
2026-06-09 16:35:38 +0800

    revised Max workshop

--- GetStartedWorkshop/src/main/kotlin/iss/nus/edu/sg/getstarted/Max.kt
+++ GetStartedWorkshop/src/main/kotlin/iss/nus/edu/sg/getstarted/Max.kt
@@ -1,12 +1,12 @@
 package iss.nus.edu.sg.iss.nus.edu.sg.getstarted
 
 fun main() {
-    val arr = intArrayOf(1,2,2,5,3,4,5,5,2)
+    val arr = arrayOf(1,2,2,5,3,4,5,5,2)
     max(arr)
 
 }
 
-fun max(arr: IntArray) {
+fun max(arr: Array<Int>) {
     if (arr.isEmpty()) println("the array is empty")
 
     var count = 0;
