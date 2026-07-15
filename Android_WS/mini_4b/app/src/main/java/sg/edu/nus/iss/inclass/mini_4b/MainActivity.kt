ee127ec628b261a1cd5cacfa16702c59b9ea23e2
loh-chunheng <chunheng.loh@gmail.com>
2026-07-15 14:31:51 +0800

    created mini_4b

---
+++ Android_WS/mini_4b/app/src/main/java/sg/edu/nus/iss/inclass/mini_4b/MainActivity.kt
@@ -0,0 +1,49 @@
+package sg.edu.nus.iss.inclass.mini_4b
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // Track the toggle state
+    private var isMusicPlaying = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, MusicService::class.java)
+
+            if (!isMusicPlaying) {
+                // 1. If not playing, change text/UI and send PLAY command
+                button.text = "Stop Music"
+                intent.action = "ACTION_PLAY"
+                isMusicPlaying = true
+            } else {
+                // 2. If already playing, reset text/UI and send STOP command
+                button.text = "Play Random MP3"
+                intent.action = "ACTION_STOP"
+                isMusicPlaying = false
+            }
+
+            startService(intent)
+        }
+
+    }
+}
